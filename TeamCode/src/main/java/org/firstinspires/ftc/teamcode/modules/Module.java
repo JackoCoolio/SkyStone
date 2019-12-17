badd661d0ce24b212f3b54429dac7881f9b65d93
JackoCoolio <g60620205h>
2019-12-16 19:45:56 -0600

    Removed old code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/Module.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/Module.java
@@ -9,7 +9,7 @@ public abstract class Module {
 
     protected final HardwareMap hardwareMap;
     protected final Telemetry telemetry;
-    protected Gamepad gamepad1, gamepad2;
+    Gamepad gamepad1, gamepad2;
 
     public Module(HardwareMap hardwareMap, Gamepad gamepad1, Gamepad gamepad2, Telemetry telemetry) {
         this.hardwareMap = hardwareMap;
@@ -21,14 +21,6 @@ public abstract class Module {
 
         init();
     }
-
-//    protected Module(ModuleParameters parameters) {
-//        this.hardwareMap = parameters.hardwareMap;
-//        this.gamepad1 = parameters.gamepad1;
-//        this.gamepad2 = parameters.gamepad2;
-//        this.telemetry = parameters.telemetry;
-//        init();
-//    }
 
     public abstract void init();
     public void init_loop() {}
